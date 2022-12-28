f6eb19e838135443ad289540a8468bdb52126f64
Greg <gmgordon15@gmail.com>
2022-12-28 17:02:22 -0500

    Added Functionality to application.  You can now search for a country and it will show various stats and facts about that country.

--- app.js
+++ app.js
@@ -4,13 +4,53 @@ const resultsContainer = document.querySelector(".results-container")
 
 
 
-inputButton.addEventListener('click', function(){
+inputButton.addEventListener('click', function(e){
+    e.preventDefault
     let countryName = countryInput.value
-    const url = `https://restcountries.com/v2/name/${countryName}?fullText=true`
+    const url = `https://restcountries.com/v2/name/${countryName}`
     console.log(countryName)
     console.log(url)
     fetch(url).then((res) => res.json())
     .then((data) =>{
         console.log(data)
+        let name = data[0].name
+        let population = data[0].population
+        let currency = data[0].currencies[0].code
+        let flag = data[0].flag
+        let language = data[0].languages[0].name
+        let capital = data[0].capital
+        let continent = data[0].subregion
+
+
+
+
+        resultsContainer.innerHTML= `
+
+        <h2 class="Name">${name}</h2>
+<img src="${flag}" alt="">
+            
+
+
+           
+<h2>Currency: ${currency}</h2>
+           
+<h2>Language: ${language}</h2>
+           
+<h2>Capital: ${capital}</h2>
+           
+<h2>Continent: ${continent}</h2>
+<h2>Population: ${population}</h2>
+`
+
+
     })
+
+
+
+    
 })
+
+
+
+
+
